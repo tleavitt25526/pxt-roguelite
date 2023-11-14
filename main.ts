a7b95d77cd42d0ddc1287e0f1df99797f2737c0c
tleavitt25526 <98976857+tleavitt25526@users.noreply.github.com>
2023-11-14 18:10:39 -0500

    Adding items that change player stats

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-//% color="#78FFA7"
-namespace test {
-    export function helloWorld() {
+namespace Roguelike {
 
+    //% block="create apple"
+    //% blockSetVariable=item
+    export function createMaxHealth(): Apple {
+        return new Apple()
     }
+}
 
+//% blockNamespace=Roguelike
+class Apple {
+    _health: number
 
+    //% block="set $this to $health"
+    //% this.defl=item
+    //% this.shadow=variables_get
+    //% health.shadow=number
+    //% health.defl=10
+    setHealth(health: number) {
+        this._health = health
+    }
 }
